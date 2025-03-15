be3c6a95aa4de74cd824efb00e5b386c827b982e
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-03-15 21:01:39 +0000

    coding_challenge_8

--- coding_challenges.js
+++ coding_challenges.js
@@ -100,3 +100,20 @@
 //     }'s (${john.calcBMI()})!`
 //   );
 // }
+
+//CHALLENGE 8
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
+}
+
+console.log(tips, totals);
