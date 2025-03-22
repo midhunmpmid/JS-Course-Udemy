18f2ee40446eadae706fad7de5f7fb21711ff21e
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-03-22 21:06:41 +0000

    addEventListner()

--- projects/guess_my_number/script.js
+++ projects/guess_my_number/script.js
@@ -1,11 +1,20 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number..🎊';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number..🎊';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 12;
-document.querySelector('.score').textContent = 18;
+// document.querySelector('.number').textContent = 12;
+// document.querySelector('.score').textContent = 18;
 
-document.querySelector('.guess').value = 50;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 50;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No guess..⛔';
+  }
+});
