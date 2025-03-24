fcaaea18ed1b01c0067d471f2e7bf1a2e87c8662
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-03-24 20:24:06 +0000

    play_again_button

--- projects/guess_my_number/script.js
+++ projects/guess_my_number/script.js
@@ -11,8 +11,7 @@
 // console.log(document.querySelector('.guess').value);
 
 let score = 20;
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,6 +24,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //when player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number..🎊';
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -49,4 +49,18 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game..🤯';
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
+  document.querySelector('.score').textContent = score;
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.guess').value = 0;
 });
