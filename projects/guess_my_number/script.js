ad68be92a994db89da561f38712a780a08616a52
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-03-24 19:54:49 +0000

    game_logic

--- projects/guess_my_number/script.js
+++ projects/guess_my_number/script.js
@@ -10,11 +10,40 @@
 // document.querySelector('.guess').value = 50;
 // console.log(document.querySelector('.guess').value);
 
+let score = 20;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No guess..⛔';
+
+    //when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number..🎊';
+
+    //when the guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too high..📉';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game..🤯';
+    }
+
+    //when the guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      document.querySelector('.message').textContent = 'Too low..📈';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game..🤯';
+    }
   }
 });
