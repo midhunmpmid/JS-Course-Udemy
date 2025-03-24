742114af12f30f5b8b40414cff3d8ce3c9c8ef32
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-03-24 20:32:26 +0000

    high_score

--- projects/guess_my_number/script.js
+++ projects/guess_my_number/script.js
@@ -12,6 +12,7 @@
 
 let score = 20;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -28,6 +29,12 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    // highscore
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     //when the guess is too high
   } else if (guess > secretNumber) {
@@ -51,6 +58,7 @@ document.querySelector('.check').addEventListener('click', function () {
   }
 });
 
+//play again
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
