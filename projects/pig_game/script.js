9ccb627b8b9a0ee5ca051b56a9b441acda4f02d4
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-03-29 14:40:34 +0000

    resetting the game

--- projects/pig_game/script.js
+++ projects/pig_game/script.js
@@ -16,15 +16,28 @@ const current1El = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-// starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-let currentScore = 0;
-let activePlayer = 0;
-const scores = [0, 0];
-let playing = true;
+// starting conditions
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -58,6 +71,7 @@ btnRoll.addEventListener('click', function () {
   }
 });
 
+// holding the score
 btnHold.addEventListener('click', function () {
   if (playing) {
     // 1.add current score to active player's score
@@ -66,7 +80,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
 
     // 2.check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       // finish the game
       playing = false;
 
@@ -85,3 +99,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+// resetting the game
+btnNew.addEventListener('click', init);
