6f638a310fd23f75a4ea00469dbf91d5bef39c1d
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-04-20 12:24:19 +0100

    coding_challenge_11_strings

--- coding_challenges.js
+++ coding_challenges.js
@@ -152,3 +152,28 @@
 // };
 
 // printForcast(arr);
+
+//CHALLENGE 11
+
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// /*
+// Desired output:
+
+// 🔴 Delayed Departure FAO TXL (11h25)
+//               Arrival BRU FAO (11h45)
+//   🔴 Delayed Arrival HEL FAO (12h05)
+//             Departure FAO LIS (12h30)
+// */
+
+// const getcode = (str) => str.slice(0, 3).toUpperCase();
+
+// for (const flight of flights.split("+")) {
+//   const [type, from, to, time] = flight.split(";");
+//   const output = `${type.startsWith("_Delayed") ? "🔴" : ""}${type.replaceAll(
+//     "_",
+//     " "
+//   )} ${getcode(from)} ${getcode(to)} (${time.replace(":", "h")})`.padStart(36);
+//   console.log(output);
+// }
