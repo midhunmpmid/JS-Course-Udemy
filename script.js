415fa6e4f2e4bbe0dd3fbe259d9fbebbabff694f
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-04-20 16:58:47 +0100

    callback_functions

--- script.js
+++ script.js
@@ -515,3 +515,24 @@ because a copy of the actual value is passed.
 So in short: JavaScript does not have true pass-by-reference, 
 but references to objects can be passed by value.
 */
+
+// callback functions
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
