2efc4c71fd334d613069a5b1979b51c649d3d7d4
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-04-23 21:12:19 +0100

    challenge_12

--- coding_challenges.js
+++ coding_challenges.js
@@ -177,3 +177,22 @@
 //   )} ${getcode(from)} ${getcode(to)} (${time.replace(":", "h")})`.padStart(36);
 //   console.log(output);
 // }
+
+//CHALLENGE 12 (Array methods)
+
+// const checkDogs = function (dogsJulia, dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice();
+//   dogsJuliaCorrected.splice(0, 1);
+//   dogsJuliaCorrected.splice(-2);
+
+//   const dogs = dogsJuliaCorrected.concat(dogsKate);
+//   console.log(dogs);
+
+//   dogs.forEach(function (dog, i) {
+//     dog >= 3
+//       ? console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`)
+//       : console.log(`Dog number ${i + 1} is still a puppy..🐶`);
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
