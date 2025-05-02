839d0f05d9902b09249d772e0531164b44d41063
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-05-02 21:42:43 +0100

    toFixed()

--- projects/bankist_array/script.js
+++ projects/bankist_array/script.js
@@ -77,7 +77,7 @@ const displayMovements = function (movements, sort = false) {
       i + 1
     } ${type}</div>
     
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov.toFixed(2)}</div>
     </div>
     `;
 
@@ -87,26 +87,26 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `£${acc.balance}`;
+  labelBalance.textContent = `£${acc.balance.toFixed(2)}`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `£${incomes}`;
+  labelSumIn.textContent = `£${incomes.toFixed(2)}`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `£${Math.abs(out)}`;
+  labelSumOut.textContent = `£${Math.abs(out).toFixed(2)}`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter(int => int >= 1)
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `£${interest}`;
+  labelSumInterest.textContent = `£${interest.toFixed(2)}`;
 };
 
 const createUsernames = function (accs) {
