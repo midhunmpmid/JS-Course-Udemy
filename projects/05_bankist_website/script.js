1ba9e5d717fe7409e38410707d670c0de54f4c4e
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-05-12 19:28:48 +0100

    menu-fade-anmation

--- projects/05_bankist_website/script.js
+++ projects/05_bankist_website/script.js
@@ -102,3 +102,28 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// menu fade animation
+const nav = document.querySelector('.nav');
+
+//using callback function to pass the argument to the handler
+
+// const handleHover = function (e, opacity) {
+//   if (e.target.classList.contains('nav__link')) {
+//     const link = e.target;
+//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//     const logo = link.closest('.nav').querySelector('img');
+
+//     siblings.forEach(el => {
+//       if (el !== link) el.style.opacity = opacity;
+//     });
+//     logo.style.opacity = opacity;
+//   }
+// };
+
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1);
+// });
