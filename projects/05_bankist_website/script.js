f118b529a56322f214bf1a3098ce60025abfedf0
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-05-13 20:31:12 +0100

    reveal-sections

--- projects/05_bankist_website/script.js
+++ projects/05_bankist_website/script.js
@@ -165,3 +165,22 @@ const headerObeserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObeserver.observe(header);
+
+//reveal seactions
+const allSections = document.querySelectorAll('.section');
+const revealSection = (entries, observer) => {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
