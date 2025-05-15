812cffd2c0465567e670ff7be62d1194a4a27ccb
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-05-15 20:32:28 +0100

    OOPS

--- script.js
+++ script.js
@@ -795,44 +795,46 @@ setInterval(function () {
 
 //creating and inserting elements
 
-const header = document.querySelector(".header");
-const message = document.createElement("div");
-message.classList.add("cookie-message");
+// const header = document.querySelector(".header");
+// const message = document.createElement("div");
+// message.classList.add("cookie-message");
 
-message.innerHTML = "We use cookies. <button>Got it.</button>";
-header.prepend(message);
+// message.innerHTML = "We use cookies. <button>Got it.</button>";
+// header.prepend(message);
 
 //deleting elements
 
-document
-  .querySelector(".btn--close--cookie")
-  .addEventListener("click", function () {
-    message.remove();
-  });
+// document
+//   .querySelector(".btn--close--cookie")
+//   .addEventListener("click", function () {
+//     message.remove();
+//   });
 
 //styles
 
-message.style.backgroundColor = "#37383d";
-message.style.width = "120%";
+// message.style.backgroundColor = "#37383d";
+// message.style.width = "120%";
 
-console.log(message.style.color);
-console.log(message.style.backgroundColor);
+// console.log(message.style.color);
+// console.log(message.style.backgroundColor);
 
-console.log(getComputedStyle(message).color);
-console.log(getComputedStyle(message).height);
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
 
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height) + 30 + "px";
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height) + 30 + "px";
 
 //attributes
 
-const logo = document.querySelector(".nav__logo");
-console.log(logo.alt);
-console.log(logo.src);
-console.log(logo.className);
+// const logo = document.querySelector(".nav__logo");
+// console.log(logo.alt);
+// console.log(logo.src);
+// console.log(logo.className);
 
 //classes
-logo.classList.add("class");
-logo.classList.remove("class");
-logo.classList.toggle("class");
-logo.classList.contains("class");
+// logo.classList.add("class");
+// logo.classList.remove("class");
+// logo.classList.toggle("class");
+// logo.classList.contains("class");
+
+///////////////// OOPS//////////////////////////
