72685758ee8fc74b84ce55de4c5fa76c51489fd1
midhunmpmid <midhunmoothedathpadi@gmail.com>
2025-06-30 21:02:50 +0100

    OOPS

--- script.js
+++ script.js
@@ -841,3 +841,16 @@ setInterval(function () {
 
 // A class is a blueprint for creating objects with specific properties and methods.
 // An instance is an object created from a class using the new keyword.
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person("Jonas", 1991);
+console.log(jonas);
+
+// 1. New {empty object} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
